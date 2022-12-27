49e29a6d9b30677bd23e0cf3a77043be452d8650
Marie Cari <mcari@nyc.yearup.org>
2022-12-27 16:50:24 -0500

    basic GUI frame created

---
+++ GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+
+    GUI() {
+        JFrame frame = new JFrame();
+        frame.setSize(100,100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
